618351c969c11072b47e5f230b8a95285b7c49a4
DDIDUs <92288706+DDIDUs@users.noreply.github.com>
2024-02-07 09:49:43 +0900

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
-for mode in 1 2
+for mode in 1 2 3 4 5
 do
     python3 evaluate.py \
         -train_mode $mode \
-        -repeat_num 2 \
-        &> log.$mode
+        -repeat_num 5 \
+        &> eval_log.$mode
 done
