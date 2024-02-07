bb606a325a2e2fb7a73c2cf56800079e331b59b0
DDIDUs <92288706+DDIDUs@users.noreply.github.com>
2024-02-07 09:50:02 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-for mode in 1 2
+for mode in 1 2 3 4 5
 do
     python3 main.py \
         -train_mode $mode \
-        -repeat_num 2 \
-        &> log.$mode
+        -repeat_num 5 \
+        &> train_log.$mode
 done
