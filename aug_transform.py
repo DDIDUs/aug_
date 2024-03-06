1894faad5e325f819fec424dccecafeebf81cb49
DDIDUs <92288706+DDIDUs@users.noreply.github.com>
2024-03-06 10:21:55 +0900

    Update aug_transform.py

--- aug_transform.py
+++ aug_transform.py
@@ -1,9 +1,9 @@
 import torchvision.transforms as transforms
 from torchvision.transforms import AutoAugment, AutoAugmentPolicy
+from autoaug import CIFAR10Policy
 
 transform_original = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
 
 # rc: random choice 
@@ -15,7 +15,6 @@ transform_aug0_rc = transforms.Compose([
                 transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
                 ]),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
 
 # aug: 우리가 일반적으로 augmentation 이라고 하는 변형 
@@ -25,18 +24,16 @@ transform_aug1 = transforms.Compose([
     transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 transform_autoaug = transforms.Compose([
-            AutoAugment(policy=AutoAugmentPolicy.CIFAR10),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ])
+    transforms.RandomHorizontalFlip(),
+    CIFAR10Policy(),
+    transforms.ToTensor(),
+])
 
 transform_uniform = transforms.Compose([
-    transforms.RandomCrop(96, padding=4),
+    #transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
